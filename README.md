ff2e29d2cce136e5e2fddaa4428bfb6b730d97d2
Yvette <Yvette@macs-MacBook-Air.local>
2018-01-28 19:57:40 -0800

    Added css. Gif click feature isn't working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gifGenerator
+
+__Description:__ This is a gif generator that pulls from Giphy's api. Click the buttons to display 10 reaction gifs. Feel free to search for our own reaction gifs using the input box.
+
+_This gif generator was created as an assignment for UCB Extension's Coding Boot Camp_
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,6 @@
 $(document).ready(function() {
     var gifArr = ["Trending", "Oh no", "But why", "Yesss", "Nooo", "Ughh", "LOL", "Huh", "Deal with it", "Hungry"];
 
-
     function displayGifs() {
         var gifSearch = $(this).attr("data-name");
 
@@ -16,29 +15,38 @@ $(document).ready(function() {
             method: "GET"
         }).done(function(response) {
 
-        	var gifDiv = $("<div class='gif'>");
+        	var gifDiv = $("<div>");
 
-        	for(var i = 0; i < 10; i++){
+        	for(var i = 0; i < response.data.length; i++){
 	        	var gifRating = response.data[i].rating;
-				var p = $("<p>").text("Rated:" + gifRating);
+				var p = $("<p>").text("Rated: " + gifRating);
 	        	gifDiv.append(p);
 
-	        	var actualGif = response.data[i].images.fixed_height.url;
-	        	console.log(actualGif);
+	        	var newGif = $("<img>").attr("src", stillGif);
+	        	newGif.attr("class", "gif");
 
-	        	var newGif = $("<img>").attr("src", actualGif);
+				var movingGif = response.data[i].images.fixed_height.url;
+	        	newGif.attr("data-moving", movingGif);
+	        	newGif.attr("data-state", "moving");
+	        	
+
+	        	var stillGif = response.data[i].images.fixed_height_still.url;
+	        	newGif.attr("data-still", stillGif);
+	        	newGif.attr("data-state", "still");
+
 	        	gifDiv.append(newGif);
         	}
 
         	$("#gif-display").html(gifDiv);
 
-        })
+        });
     }
 
     //Goes through gifArr to create buttons dynamically
     function displayButtons(){
     	$("#gif-buttons").empty();
 
+    	// Generates buttons in gifArr
     	for(var i = 0; i < gifArr.length; i++){
     		var buttons = $("<button>");
 
@@ -49,19 +57,32 @@ $(document).ready(function() {
     		$("#gif-buttons").append(buttons);
     	}
     }
-
+    // Pushes new gifs into the gifArr from the submission box
     $("#add-gif").on("click", function(event){
     	event.preventDefault();
 
     	var gifInput = $("#gif-input").val().trim();
-
-    	gifArr.push(gifInput);
-
-    	console.log(gifArr);
+		gifArr.push(gifInput);
 
     	displayButtons();
-    })
+    });
 
     displayButtons();
+
+    // Makes call to API to display gifs when button is clicked
     $(document).on("click", ".gif", displayGifs);
-})
+
+    // Triggers between still and moving gif when clicked on
+    $(".gif").on("click", function(){
+    	// debugger;
+    	if($(this).attr("data-state") === "still"){
+    		$(".gif").attr("src", "data-moving");
+    		$(".gif").attr("data-state", "moving");
+    		console.log("still");
+    	}else if($(this).attr("data-state") === "moving"){
+    		$(".gif").attr("src", "data-still");
+    		$(".gif").attr("data-state", "still");
+    		console.log("moving");
+    	}
+    });
+});
